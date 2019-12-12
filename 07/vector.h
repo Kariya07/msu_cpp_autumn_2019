23d23c81133f432e9d7ad9521d856cdd72e85f2b
Kariya07 <karry0799@mail.ru>
2019-12-12 16:54:36 +0300

    Vector

--- 07/vector.h
+++ 07/vector.h
@@ -32,7 +32,7 @@ public:
 };
 
 template<class T>
-class Iterator : public std::iterator<std::forward_iterator_tag, T> {
+class Iterator : public std::iterator<std::random_access_iterator_tag, T> {
     T *ptr_;
 public:
     explicit Iterator(T *ptr) : ptr_(ptr) {}
@@ -75,17 +75,17 @@ public:
     using allocator_type = Alloc;
 
     size_type N;
-    allocator_type _data;
+    allocator_type _alloc;
     pointer current_ptr;
 
     void buf_resize(size_type newSize) {
-        if (_data.max_size() < newSize) {
-            pointer newData = _data.allocate(newSize);;
-            for (size_type i = 0; i < N; i++) {
-                _data.construct(newData + i, current_ptr[i]);
-                _data.destroy(current_ptr + i);
+        if (_alloc.max_size() < newSize) {
+            pointer newData = _alloc.allocate(newSize);;
+            for (size_type i = 0; i < _alloc.max_size(); i++) {
+                new (newData + i) value_type(current_ptr[i]);
+                _alloc.destroy(current_ptr + i);
             }
-            _data.deallocate(current_ptr);
+            _alloc.deallocate(current_ptr);
             current_ptr = newData;
             return;
         }
@@ -109,34 +109,34 @@ public:
     }
 
     explicit Vector(size_type count = 1024) {
-        current_ptr = _data.allocate(count);
+        current_ptr = _alloc.allocate(count);
         N = 0;
     }
 
     Vector(size_type count, const value_type &defaultValue) {
-        current_ptr = _data.allocate(count);
+        current_ptr = _alloc.allocate(count);
         for (size_type i = 0; i < count; i++) {
-            _data.construct(current_ptr + i, std::move(defaultValue));
+            _alloc.construct(current_ptr + i, std::move(defaultValue));
         }
         N = count;
     }
 
     Vector(std::initializer_list<value_type> init) : N(init.size()) {
-        current_ptr = _data.allocate(init.size());
+        current_ptr = _alloc.allocate(init.size());
         size_t i = 0;
         auto current = init.begin();
         const auto end = init.end();
         while (current != end) {
-            _data.construct(current_ptr + i, std::move(*current++));
+            _alloc.construct(current_ptr + i, std::move(*current++));
             i++;
         }
     }
 
     ~Vector() {
         for (size_type i = 0; i < N; i++) {
-            _data.destroy(current_ptr + i);
+            _alloc.destroy(current_ptr + i);
         }
-        _data.deallocate(current_ptr);
+        _alloc.deallocate(current_ptr);
     }
 
     reverse_iterator rbegin() noexcept {
@@ -156,25 +156,25 @@ public:
     }
 
     void push_back(value_type &&value) {
-        if (N == _data.max_size()) {
+        if (N == _alloc.max_size()) {
             buf_resize(2 * N);
         }
-        _data.construct(current_ptr + N, std::move(value));
+        _alloc.construct(current_ptr + N, std::move(value));
         N++;
     }
 
     void push_back(const value_type &value) {
-        if (N == _data.max_size()) {
+        if (N == _alloc.max_size()) {
             buf_resize(2 * N);
         }
-        _data.construct(current_ptr + N, std::move(value));
+        _alloc.construct(current_ptr + N, std::move(value));
         N++;
     }
 
     void pop_back() {
         if (N != 0) {
             N--;
-            _data.destroy(current_ptr + N);
+            _alloc.destroy(current_ptr + N);
         } else {
             throw std::out_of_range("pop_back");
         }
@@ -189,25 +189,25 @@ public:
     }
 
     void reserve(size_type count) {
-        size_type my_capacity = _data.max_size();
+        size_type my_capacity = _alloc.max_size();
         if (my_capacity < count) {
             buf_resize(count);
         }
     }
 
     size_type capacity() const noexcept {
-        return _data.max_size();
+        return _alloc.max_size();
     }
 
     void resize(size_type newsize) {
         if (newsize < N) {
             for (size_type i = newsize; i < N; i++) {
-                _data.destroy(current_ptr + i);
+                _alloc.destroy(current_ptr + i);
             }
             N = newsize;
         } else {
             if (newsize > N) {
-                size_type my_capacity = _data.max_size();
+                size_type my_capacity = _alloc.max_size();
                 if (newsize > my_capacity) {
                     buf_resize(newsize);
                 }
@@ -222,16 +222,17 @@ public:
         if (newsize < N) {
             N = newsize;
             for (size_type i = newsize; i < N; i++) {
-                _data.destroy(current_ptr + i);
+                _alloc.destroy(current_ptr + i);
             }
         } else {
             if (newsize > N) {
-                size_type my_capacity = _data.max_size();
+                size_type my_capacity = _alloc.max_size();
                 if (newsize > my_capacity) {
                     buf_resize(newsize);
                 }
                 while (N < newsize) {
-                    current_ptr[N++] = defaultValue;
+                    _alloc.construct(current_ptr + N, std::move(defaultValue));
+                    N++;
                 }
             }
         }
