b915dd2ea20aa9580a521203e7f9b049a28dca35
Kariya07 <karry0799@mail.ru>
2019-12-16 23:02:27 +0300

    Sort

--- 09/main.cpp
+++ 09/main.cpp
@@ -8,7 +8,7 @@
 
 const size_t chunk_size = 1024 * 1024;
 
-void merge_files(int num_of_files, std::vector<std::string> names) {
+void merge_files(int num_of_files, const std::vector<std::string> &names) {
     typedef std::istream_iterator<uint64_t> It;
     typedef std::pair<It, It> Range;
     auto less_first = [](const Range &lhs, const Range &rhs) {
@@ -22,9 +22,10 @@ void merge_files(int num_of_files, std::vector<std::string> names) {
         num_of_files--;
     }
     std::ofstream out;
-    out.open("sorted.txt");
+    const std::string newFileName = "sorted.txt";
+    out.open(newFileName);
     if (!out) {
-        std::cerr << "Can't open file: sorted.txt" << '\n';
+        throw (std::runtime_error(newFileName));
     }
     while (!heap.empty()) {
         auto range = heap.top();
@@ -41,11 +42,11 @@ void BigFileSort(const std::string &FileName) {
     std::ifstream file;
     file.open(FileName);
     if (!file) {
-        std::cerr << "Can't open file: "<< FileName << '\n';
+        throw (std::runtime_error(FileName));
     }
 
     size_t num_of_files = 0;
-    uint64_t *buffer = new uint64_t[chunk_size]; //8byte*1024*1024 = 8kb*1024 = 8 mb
+    auto *buffer = new uint64_t[chunk_size]; //8byte*1024*1024 = 8kb*1024 = 8 mb
     std::vector<std::string> names;
     while (file) {
         file.read(reinterpret_cast<char *>(buffer), chunk_size);
@@ -67,7 +68,8 @@ void BigFileSort(const std::string &FileName) {
             std::ofstream out;
             out.open(names[num_of_files]);
             if (!out) {
-                std::cerr << "Can't open file: "<< names[num_of_files] << '\n';
+                delete[] buffer;
+                throw (std::runtime_error(names[num_of_files]));
             }
             for (size_t i = 0; i < read_size / 2; i++) {
                 out << buffer[i] << ' ';
@@ -75,7 +77,8 @@ void BigFileSort(const std::string &FileName) {
             out.close();
             out.open(names[num_of_files + 1]);
             if (!out) {
-                std::cerr << "Can't open file: "<< names[num_of_files + 1] << '\n';
+                delete[] buffer;
+                throw (std::runtime_error(names[num_of_files + 1]));
             }
             for (size_t i = middle; i < read_size; i++) {
                 out << buffer[i] << ' ';
@@ -89,22 +92,24 @@ void BigFileSort(const std::string &FileName) {
     merge_files(num_of_files, names);
 }
 
-uint64_t getNum() {
-    return std::rand() % 1000;
-}
-
 int main() {
     const std::string FileName = "test.txt";
     std::ofstream newfile(FileName);
     if (!newfile) {
-        std::cerr << "Can't open file: "<< FileName << '\n';
+        std::cerr << "Can't open file: " << FileName << '\n';
         return 1;
     }
     const size_t exprNum = 1000;
     for (size_t i = 0; i < exprNum; i++) {
-        newfile.write(reinterpret_cast<const char *>(new uint64_t(getNum())), sizeof(uint64_t));
+        uint64_t num = std::rand() % 1000;
+        newfile.write((char *) &num, sizeof(uint64_t));
     }
     newfile.close();
-    BigFileSort(FileName);
+    try {
+        BigFileSort(FileName);
+    } catch (const std::runtime_error &err) {
+        std::cerr << "Can't open file: " << err.what() << '\n';
+        return 1;
+    }
     return 0;
 }
